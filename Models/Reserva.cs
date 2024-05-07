743a0123e866853b81ced1e7d57aa4179980cdcb
Vinicius Cerqueira <viniciuscerqueira070@gmail.com>
2024-05-07 17:47:02 -0300

    Desafio de um Sistema de Hospedagem utilizando .NET Concluido!

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,12 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
-            {
-                Hospedes = hospedes;
+            if(this.Suite != null && hospedes.Count <= this.Suite.Capacidade){
+                this.Hospedes = hospedes;
+                
             }
-            else
-            {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+            else{
+                throw new InvalidOperationException("Número de hóspedes excede a capacidade da suíte.");
             }
         }
 
@@ -35,26 +31,17 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            return this.Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
-
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
-            {
-                valor = 0;
+            
+            decimal valorDiaria = this.Suite?.ValorDiaria ?? 0;
+            if(this.DiasReservados >= 10){
+                valorDiaria *= 0.9M;
             }
-
-            return valor;
+            return valorDiaria;
         }
     }
 }
